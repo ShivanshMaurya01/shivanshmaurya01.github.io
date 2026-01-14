7590572390b2d7cd4d233f5fe0230ef66b680247
ShivanshMaurya01 <shivanshmaurya291@gmail.com>
2026-01-14 21:35:24 +0530

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,7 @@
+export const SYSTEM_CONFIG = {
+    API_KEY: "ca155513-3932-4919-867c-f10a8867a5c7",
+    REFRESH_INTERVAL: 60000, // 1 minute
+    APP_NAME: "CricPulse Pro",
+    REGION: "Asia/Kolkata",
+    VERSION: "2.0.0-Enterprise"
+};
